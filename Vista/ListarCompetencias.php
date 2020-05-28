52167f1961905f0163bade5a6c5d268f539e5b02
Juan Felipe Garcia Duque <jfgarcia064@misena.edu.co>
2020-05-28 07:50:28 -0500

    listar competencias

--- Vista/ListarCompetencias.php
+++ Vista/ListarCompetencias.php
@@ -5,7 +5,7 @@ require_once('../Modelo/CrudCompetencia.php');
 $CrudCompetencia = new CrudCompetencia();
 $ListaCompetencias = $CrudCompetencia->ListarCompetencias();
 
-var_dump($ListaCompetencias);
+//var_dump($ListaCompetencias);
 
 ?>
 <!DOCTYPE html>
@@ -17,13 +17,22 @@ var_dump($ListaCompetencias);
 </head>
 <body>
      <h1 align="center">Listado de Competencias</h1>
-     <table align="center">
+     <table align="center" border="1">
           <thead>
                <tr><th>Codigo Competencia</th><th>Nombre Competencia</th></tr>
           </thead>
 
           <tbody>
-          
+          <?php
+               foreach ($ListaCompetencias as $Competencia) {
+                    ?>
+                    <tr>
+                    <td><?php echo $Competencia->getCodigoCompetencia();?></td>
+                    <td><?php echo $Competencia->getNombreCompetencia();?></td>
+
+                    <?php
+               }
+          ?>
           </tbody>
      
      </table>
